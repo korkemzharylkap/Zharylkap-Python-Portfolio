bc477afe9f0e66d31fe8e4fbf540fcabe7b50848
Korkem Zharylkap <168582738+korkemzharylkap@users.noreply.github.com>
2025-02-09 21:26:45 -0500

    Update main.py

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,15 +1,1 @@
-pip install streamlit
-import streamlit as st
-import pandas as pd
-streamlit run basic_streamlit_app/main.py
-st.title("Penguins Data Set")
-# Interactive Filtering
-species = st.selectbox("Select Species:", df["species"].unique())
-island = st.multiselect("Select Island(s):", df["island"].unique(), default=df["island"].unique())
-min_bill_length = st.slider("Minimum Bill Length (mm):", float(df["bill_length_mm"].min()), float(df["bill_length_mm"].max()), float(df["bill_length_mm"].min()))
 
-# Apply filters
-filtered_df = df[(df["species"] == species) & (df["island"].isin(island)) & (df["bill_length_mm"] >= min_bill_length)]
-
-st.write(f"### Displaying {species} Penguins")
-st.dataframe(filtered_df)

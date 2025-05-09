e6afeea3a87b8ffc48742819003481005fe4b5f1
Korkem Zharylkap <168582738+korkemzharylkap@users.noreply.github.com>
2025-05-09 01:22:05 -0400

    Update makeup_app.py

--- StreamlitAppFinal/makeup_app.py
+++ StreamlitAppFinal/makeup_app.py
@@ -10,14 +10,13 @@ import requests
 st.set_page_config(page_title="💄 Makeup Ingredient Analyzer", layout="centered")
 
 # File paths
-main_url = "https://raw.githubusercontent.com/korkemzharylkap/Zharylkap-Python-Portfolio/main/StreamlitAppFinal/ingredient_database.json"
-user_url = "https://raw.githubusercontent.com/korkemzharylkap/Zharylkap-Python-Portfolio/main/StreamlitAppFinal/user_ingredients.json"
 
-main_resp = requests.get(main_url)
-MAIN_DB_FILE = json.loads(main_resp.text)
-print(MAIN_DB_FILE)
+# Specify the path to your JSON file
+file_path = 'ingredient_database.json'
 
-user_resp = requests.get(user_url)
-USER_DB_FILE = json.loads(user_resp.text)
-print(USER_DB_FILE)
-    
+# Open the JSON file and load its contents
+with open(file_path, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Print the data to check what has been loaded
+print(data)

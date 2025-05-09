d906400a716cb8346c42db60846e06f1a1927416
Korkem Zharylkap <168582738+korkemzharylkap@users.noreply.github.com>
2025-05-09 02:17:57 -0400

    Update makeup_app.py

--- StreamlitAppFinal/makeup_app.py
+++ StreamlitAppFinal/makeup_app.py
@@ -7,8 +7,8 @@ import re
 # Set Streamlit page configuration
 st.set_page_config(page_title="💄 Makeup Ingredient Analyzer", layout="centered")
 
-MAIN_DB_PATH = 'StreamlitAppFinal/ingredients_database.csv'
+df = 'StreamlitAppFinal/ingredients_database.csv'
 USER_DB_PATH = 'StreamlitAppFinal/user_ingredients.csv'
 
 
-st.write("First few rows of the dataset:", MAIN_DB_PATH.head())
+st.write("First few rows of the dataset:", df.head())
